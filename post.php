57a10c80880ddf3786494b99e776d56d5ba6fe67
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 16:33:23 +1000

    edited search post

--- post.php
+++ post.php
@@ -158,11 +158,11 @@
 			try {
 				echo "user id : " .$userId;
 				echo "title is  : " .$title;
-				$sql = 'SELECT * FROM posts WHERE (title like :title ) AND (user_id = :userId)';
+				$sql = 'SELECT * FROM posts WHERE (title like :title ) ';
 				$stmt1 = $this->dbConn->prepare($sql);
 				$stmt1->bindValue(':title', '%'. $title . '%' );
 				echo " reched hee ";
-				$stmt1->bindParam(':userId', $userId);
+				//$stmt1->bindParam(':userId', $userId);
 				$stmt1->execute();
 				$post = $stmt1->fetch(PDO::FETCH_ASSOC);
 				echo $post;
