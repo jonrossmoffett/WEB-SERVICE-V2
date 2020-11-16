9fb838f20253b811a45d582a271be71105940bb0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 13:01:10 +1000

    locked off the index page

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <br>
 
 <h1>https://phpwebservice.herokuapp.com/api/createPost</h1>
+<h2>Request Type: POST</h2>
+<h2>headers: Authorization : JWT</h2>
 <h2>Arguements:</h2>
 <h3>"title" : "this is an example title"</h3>
 <h3>"description" : "this is an example description for the post"</h3>
@@ -18,16 +20,26 @@
 <br>
 
 <h1>https://phpwebservice.herokuapp.com/api/deletePost</h1>
-
+<h2>Request Type: POST</h2>
+<h2>headers: Authorization : JWT</h2>
 <h2>Arguements:</h2>
-
 <h3>"postId" : 1</h3>
 
 <br>
 
 <h1>https://phpwebservice.herokuapp.com/api/updatePost</h1>
-
+<h2>Request Type: POST</h2>
+<h2>headers: Authorization : JWT</h2>
 <h2>Arguements:</h2>
 <h3>"postId" : 1</h3>
 <h3>"title" : "example title for update"</h3>
 <h3>"description" : "example description for updating post"</h3>
+
+<br>
+
+<h1>https://phpwebservice.herokuapp.com/api/updatePost</h1>
+<h2>Request Type: GET</h2>
+<h2>headers: Authorization : JWT</h2>
+<h2>Arguements:</h2>
+
+<h3></h3>
