3f40e177684cf67fd5925f6624259b26b6d20ded
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 13:18:41 +1000

    changed deletePost.php from POST request to DELETE request

--- api/deletePost.php
+++ api/deletePost.php
@@ -17,7 +17,7 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
     http_response_code(200);
   }
   if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: DELETE, OPTIONS");
     //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
     $request = Request::createFromGlobals();
     $response = new Response();
@@ -29,7 +29,7 @@ if (isset($_SERVER["HTTP_ORIGIN"])) {
 
 
 $validator = new Validator;
-$validator->validateRequestType('POST');
+$validator->validateRequestType('DELETE');
 $db = new database;
 $dbConn = $db->connect();
 $data = json_decode(file_get_contents("php://input"));
