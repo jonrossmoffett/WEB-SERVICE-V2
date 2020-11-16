c0acbd7069821367e341ee51fbcca9eba63f14ed
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 18:02:52 +1000

    added databsae logs

--- logs.php
+++ logs.php
@@ -8,18 +8,18 @@ class Dblogger {
             $created_at = date('Y-m-d H:i:s');
             $updated_at = date('Y-m-d H:i:s');
             
-            echo "reached before sql command ";
-            $sql = 'INSERT INTO logs (ip, browser, action, created_at, updated_at) VALUES (:ip, :browser, :action, :created_at, :updated_at)';
-            echo "reached before pepare statement ";
-            $stmt = $this->dbConn->prepare($sql);
-            echo "reached after prepare statment ";
-            $stmt->bindParam(':ip', $ip);
-            $stmt->bindParam(':browser', $browser);
+			$sql = 'INSERT INTO logs (ip, browser, action) VALUES(:ip, :browser, :action )';
+
+			$stmt = $this->dbConn->prepare($sql);
+			$stmt->bindParam(':id', $ip);
+			$stmt->bindParam(':browser', $browser);
             $stmt->bindParam(':action', $action);
-            $stmt->bindParam(':created_at', $created_at);
-            $stmt->bindParam(':updated_at', $updated_at);
-            echo "reached before execute statement ";
-            $stmt->execute();
+
+			if($stmt->execute()) {
+				return true;
+			} else {
+				return false;
+			}
 
     
         }catch(Exception $e){
