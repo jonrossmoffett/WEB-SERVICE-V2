8a2cb0b66e43a08d89aa0594c99e263e832e6e34
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 17:59:16 +1000

    added databsae logs

--- logs.php
+++ logs.php
@@ -7,14 +7,18 @@ class Dblogger {
         try {
             $created_at = date('Y-m-d H:i:s');
             $updated_at = date('Y-m-d H:i:s');
-    
+            
+            echo "reached before sql command ";
             $sql = 'INSERT INTO logs (ip, browser, action, created_at, updated_at) VALUES (:ip, :browser, :action, :created_at, :updated_at)';
+            echo "reached before pepare statement ";
             $stmt = $this->dbConn->prepare($sql);
+            echo "reached after prepare statment ";
             $stmt->bindParam(':ip', $ip);
             $stmt->bindParam(':browser', $browser);
             $stmt->bindParam(':action', $action);
             $stmt->bindParam(':created_at', $created_at);
             $stmt->bindParam(':updated_at', $updated_at);
+            echo "reached before execute statement ";
             $stmt->execute();
 
     
