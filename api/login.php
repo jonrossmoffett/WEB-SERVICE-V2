096cbfb28043b7b083319456bed2fcb9c63813b0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 19:22:41 +1000

    added to logs table

--- api/login.php
+++ api/login.php
@@ -4,44 +4,14 @@ include_once('../jwt.php');
 include_once('../constants.php');
 include_once('../validator.php');
 include_once('../vendor/autoload.php');
-include_once('../rate.php');
+include_once('../rateLimiterConfig.php.php');
 include_once('../whitelist.php');
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
     checkDomainWhitelist($_SERVER["REMOTE_ADDR"]);
-
-    date_default_timezone_set('Australia/Brisbane');
-	session_start();
-	
-	//rate limit 1000 calls per 24 hours
-	$rateLimiter = new RateLimiter($_SERVER["REMOTE_ADDR"]);
-	$limit = 1000;				    //connection per minutes
-	$minutes = 1 * 1440;			//1 day
-	$seconds = floor($minutes * 1);	//retry after 1 day
-	try {
-		$rateLimiter->limitRequestsInMinutes($limit, $minutes);
-	} catch (RateExceededException $e) {
-		header("HTTP/1.1 429 Too Many Requests");
-		header(sprintf("Retry-After: %d", $seconds));
-		$data = 'Rate Limit Exceeded ';
-		die (json_encode($data));
-	}
-
-	//rate limit 1 call per second
-	if (isset($_SESSION['LAST_CALL'])) {
-		$last = strtotime($_SESSION['LAST_CALL']);
-		$curr = strtotime(date("Y-m-d h:i:s"));
-		$sec =  abs($last - $curr);
-		if ($sec <= 1) {
-		  $data = 'Rate Limit Exceeded';  // rate limit
-		  header('Content-Type: application/json');
-		  die (json_encode($data));        
-		}
-	}
-	$_SESSION['LAST_CALL'] = date("Y-m-d h:i:s");
-
+    runRateLimiter();
 
 if (isset($_SERVER["HTTP_ORIGIN"])) {
     header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
