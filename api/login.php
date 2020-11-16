c9174c976ba41805b791d40cec626efe3df85226
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 19:24:24 +1000

    added to logs table

--- api/login.php
+++ api/login.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
     date_default_timezone_set('Australia/Brisbane');
 	session_start();
 
-    runRateLimiter();
+    //runRateLimiter();
 
 if (isset($_SERVER["HTTP_ORIGIN"])) {
     header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
