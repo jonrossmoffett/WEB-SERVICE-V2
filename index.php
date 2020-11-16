2fe2f3f4ede30d5dc13c76417389ffbfe071559b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 12:38:56 +1000

    locked off the index page

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
   
 <?php 
 
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/* if (isset($_SERVER["HTTP_ORIGIN"])) {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Max-Age: 0");
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
+    $request = Request::createFromGlobals();
+    $response = new Response();
+    $response->setStatusCode(200);
+    $response->prepare($request);
+    $response->send();
+  }
+  header("Content-Type: application/json; charset=UTF-8"); */
+
+  $validator = new Validator;
   $validator->validateRequestType('GET');
   $validator->response(401,'You may not access the index page');
 
