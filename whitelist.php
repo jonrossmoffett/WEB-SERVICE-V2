159b6c43f8534bfcd89618384cf74fd11bb1fabb
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 19:19:05 +1000

    added to logs table

--- whitelist.php
+++ whitelist.php
@@ -3,7 +3,7 @@
 function checkDomainWhitelist($domain) {
     $whitelist       = array( '120.154.217.176', '10.63.208.77','10.73.248.115', 'a10.7.155.197' );
     if( ! in_array( $domain, $whitelist ) ) { 
-        $message = 'The domain ' . $_SERVER["REMOTE_ADDR"] . " is not allowed to access this api. Allowed domains inlcude " . implode($whitelist);
+        $message = 'The domain ' . $_SERVER["REMOTE_ADDR"] . " is not allowed to access this api. Allowed domains inlcude " . implode(', ',$whitelist);
         $response = json_encode([$message]);
         echo $response;
         die();
