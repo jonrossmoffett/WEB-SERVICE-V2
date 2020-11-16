0e4c63f22a0c72f8ed2ed1bc7d116021a035e775
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 12:54:00 +1000

    locked off the index page

--- index.php
+++ index.php
@@ -7,29 +7,27 @@
 ?>
 
 <h1>Supported Api routes</h1>
-<br>
-<h1>https://phpwebservice.herokuapp.com/api/createPost</h1>
-<br>
-<h2>Arguements:</h2>
-<br>
-<h2>title => "this is an example title"</h2>
-<br>
-<h2>title => "this is an example description for the post"</h2>
 
+<br>
+
+<h1>https://phpwebservice.herokuapp.com/api/createPost</h1>
+<h2>Arguements:</h2>
+<h2>title => "this is an example title"</h2>
+<h2>description => "this is an example description for the post"</h2>
+
+<br>
 
 <h1>https://phpwebservice.herokuapp.com/api/deletePost</h1>
-<br>
+
 <h2>Arguements:</h2>
-<br>
+
 <h2>id => 1</h2>
 
+<br>
 
 <h1>https://phpwebservice.herokuapp.com/api/updatePost</h1>
-<br>
+
 <h2>Arguements:</h2>
-<br>
 <h2>postId => 1</h2>
-<br>
 <h2>title => "example title for update"</h2>
-<br>
 <h2>description => "example description for updating post"</h2>
