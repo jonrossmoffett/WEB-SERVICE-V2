075cdcd95a363adf8b474c5d19579e60fd948fc5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 16:30:11 +1000

    edited search post

--- post.php
+++ post.php
@@ -164,7 +164,7 @@
 				echo " reched hee ";
 				$stmt1->bindValue(':userId', $userId);
 				$stmt1->execute();
-				$post = $stmt1->fetchAll(PDO::FETCH_CLASS);
+				$post = $stmt1->fetch(PDO::FETCH_ASSOC);
 				echo $post;
 				return $post;
 			}catch(Exception $e) {
