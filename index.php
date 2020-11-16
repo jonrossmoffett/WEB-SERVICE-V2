c9b5abe59ff4d4c06e125f7581e29929c96c48cd
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 12:45:13 +1000

    locked off the index page

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
   
 <?php 
 
+$createPostExample = [
+	"title" => "this is an example title",
+	"description" => "this is an example description"
+];
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+echo "Supported routes";
 
+echo "https://phpwebservice.herokuapp.com/api/createPost";
 
-if (isset($_SERVER["HTTP_ORIGIN"])) {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Max-Age: 0");
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With,Referer,User-Agent,Access-Control-Allow-Origin');
-  }
+echo json_encode($createPostExample);
 
-  if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    //if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) header("Access-Control-Allow-Headers: {" . $_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"] ."}");
-    $request = Request::createFromGlobals();
-    $response = new Response();
-    $response->setStatusCode(200);
-    $response->prepare($request);
-    $response->send();
-  }
-  header("Content-Type: application/json; charset=UTF-8");
-
- try{
-	$validator = new Validator;
-	$validator->validateRequestType('GET');
-	$validator->response(401,'You may not access the index page');
-
- }catch(Exception $e){
-	 echo $e;
- }
 
 
 ?>
