3e00f43507d8712db08cbf63209bc373dda81399
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 19:08:53 +1000

    added to logs table

--- whitelist.php
+++ whitelist.php
@@ -1,7 +1,7 @@
 <?php
 
 function checkDomainWhitelist($domain) {
-    $whitelist       = array( 'icloud.com', 'me.com' );
+    $whitelist       = array( '120.154.217.176', 'me.com' );
     if( ! in_array( $domain, $whitelist ) ) { 
         echo "your domain is not allowed to access this api";
         die();
